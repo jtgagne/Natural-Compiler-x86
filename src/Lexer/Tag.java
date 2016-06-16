cfac8ebe2a04403406ba74e752f65025820700c7
Justin Gagne <gagnej3@wit.edu>
2016-06-16 14:11:48 -0400

    Updated tag cause git won't

--- src/Lexer/Tag.java
+++ src/Lexer/Tag.java
@@ -2,41 +2,108 @@ package lexer;
 
 /**
  * Define constants for Tokens (for readability)
+ * Refactored to group like tags
  * Created by Justin Gagne on 6/5/16.
  */
 public class Tag {
+
+    //Values start at 256
+
+    //Join operators
     public final static int
         AND         = 256,
-        BASIC       = 257,
-        BREAK       = 258,
-        DO          = 259,
-        ELSE        = 260,
-        EQ          = 261,
-        FALSE       = 262,
-        GE          = 263,
-        ID          = 264,
-        IF          = 265,
-        INDEX       = 266,
-        LE          = 267,
-        MINUS       = 268,
-        NE          = 269,
-        NUM         = 270,
-        OR          = 271,
-        REAL        = 272,
-        TEMP        = 273,
-        TRUE        = 274,
-        WHILE       = 275,
-        INCREASE    = 276,
-        NOT         = 277,
-        GREATER     = 278,
-        LESS        = 279,
+        NOT         = 257,
+        OR          = 258;
+
+    //Control values
+    public final static int
+        BREAK       = 259,
+        DO          = 260,
+        FOR         = 261,
+        ELSE        = 262,
+        IF          = 263,
+        WHILE       = 264;
+
+    //Boolean values
+    public static final int
+        FALSE       = 265,
+        TRUE        = 266;
+
+    //Data types
+    public static final int
+        BASIC       = 267,
+        NUM         = 268,
+        REAL        = 269;
+
+    //Comparison operators
+    public static final int
+        EQ          = 270,
+        GE          = 271,
+        GREATER     = 272,
+        LE          = 273,
+        LESS        = 274,
+        NE          = 275,
+        THAN        = 276;
+
+    //Phrase Identification values
+    public static final int
+        ERROR       = 277,
+        INITIALIZER = 278,
+        NULL        = 279,
         PHRASE      = 280,
-        NULL        = 281,
-        TERMINAL    = 282,
-        INITIALIZER = 283,
-        ASSIGNMENT  = 284,
-        ERROR       = 285,
-        DECREASE    = 286,
-        FOR         = 287,
-        TO          = 288;
+        TERMINAL    = 281;
+
+    //Incrementation  / looping operators
+    public static final int
+        DECREASE    = 282,
+        INCREASE    = 283,
+        TO          = 284;
+
+    //Variable declaration and assignment
+    public static final int
+        ASSIGNMENT  = 285,
+        ID          = 286,
+        INDEX       = 287,
+        TEMP        = 288;
+
+    //Arithmetic operators
+    public static final int
+        MINUS       = 289;
+
+    //End of file
+    public static final int
+        EOF         = 290;
+
+    /**
+     * Check if a tag is a valid boolean value
+     * @param tag the tag of the token
+     * @return true if it is a boolean
+     */
+    public static boolean isBoolean(int tag){
+        return (tag == Tag.FALSE || tag == Tag.TRUE);
+    }
+
+    /**
+     * Check is a tag is classified as a number, real or int
+     * @param tag the tag of the token
+     * @return true if the tag is refers to a number
+     */
+    public static boolean isNumber(int tag){
+        return (tag == Tag.NUM ||
+                tag == Tag.REAL);
+    }
+
+    public static boolean isComparisonOperator(int tag){
+        return (tag == Tag.EQ ||
+                tag == Tag.GE ||
+                tag == Tag.GREATER ||
+                tag == Tag.LE ||
+                tag == Tag.LESS ||
+                tag == Tag.NE);
+    }
+
+    public static boolean isJoiningOperator(int tag){
+        return (tag == Tag.AND ||
+                tag == Tag.OR);
+    }
 }
